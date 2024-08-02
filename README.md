3dd61a0811a3885a6aa21be3dd66fbd1b118d3a9
sadik03 <mssadik03@gmail.com>
2024-08-02 11:35:47 +0530

    main

---
+++ README.md
@@ -0,0 +1,17 @@
+# firebase-google-auth
+firebase google auth , register, login , logout and reset the firebase auth with data store in firestore database
+
+How to Run the Firebase
+
+step 1 : setup the firebase access and create the SDK setup and configuration
+![image](https://user-images.githubusercontent.com/36411169/223112796-801d4409-2be2-44e3-a276-a8c33fab31e2.png)
+
+Step 2 : Create a firestore database 
+![image](https://user-images.githubusercontent.com/36411169/223113027-9c1729e6-529b-4ca7-a419-6786e778a845.png)
+
+Step 3 :Enable the Authentication by click the auth provider to google and email/password or facebook firebase auth
+![image](https://user-images.githubusercontent.com/36411169/223113129-f14d201d-f3f6-4f9a-8798-5ad3d9e46f54.png)
+
+Step 4 : Clone the projects by git clone projectname
+step 5: open the firebase.js and config the firebase 
+step 6 : and run the projects by npm start
---
+++ src/firebase.js
@@ -0,0 +1,98 @@
+import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdH-0p2fFrJXYkOrru21l5DCrpLqXNx-s",
+  authDomain: "next-sports-9dd0a.firebaseapp.com",
+  projectId: "next-sports-9dd0a",
+  storageBucket: "next-sports-9dd0a.appspot.com",
+  messagingSenderId: "697653624392",
+  appId: "1:697653624392:web:7b6b2d6859b5e2b198a27d",
+  measurementId: "G-MPF5XB9CZS"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+// google auth signin
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+// login with email and password
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+// register with email
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const logout = () => {
+  signOut(auth);
+};
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordReset,
+  logout,
+};
